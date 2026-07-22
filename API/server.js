c1070839d770ddf57a59d51664155cbf5455698d
athstack <versionversion1964@gmail.com>
2026-07-22 11:06:32 +0300

    fix: re-add ngrok header to CORS allowed headers

--- API/server.js
+++ API/server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
         res.header("Access-Control-Allow-Origin", origin);
     }
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, ngrok-skip-browser-warning");
     if (req.method === "OPTIONS") {
         return res.sendStatus(204);
     }
