2d190fd6e5d90802123f7eddf0303edbbaf517a7
athstack <versionversion1964@gmail.com>
2026-07-22 07:52:47 +0300

    Add TiDB Cloud SSL support and update env vars

--- API/server.js
+++ API/server.js
@@ -18,16 +18,22 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const db = mysql.createPool({
+const dbConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT || 3306,
+    port: process.env.DB_PORT || 4000,
     waitForConnections: true,
-    connectionLimit: 10, 
+    connectionLimit: 10,
     queueLimit: 0
-});
+};
+
+if (process.env.DB_SSL_CA) {
+    dbConfig.ssl = { ca: process.env.DB_SSL_CA };
+}
+
+const db = mysql.createPool(dbConfig);
 
 db.getConnection((err, connection) => {
     if (err) {
